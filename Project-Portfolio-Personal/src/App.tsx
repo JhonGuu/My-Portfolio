c943a270f177a117871cc927856dbedc56170677
Juan Jose Aranda <arandajuanjose17@gmail.com>
2024-01-31 23:51:09 -0300

    creacion del proyecto.
    se instalaron las dependencias, se instaló Tailwind y se inicializo el proyecto.

---
+++ Project-Portfolio-Personal/src/App.tsx
@@ -0,0 +1,20 @@
+import Home from "./views/Home"
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Home></Home>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+      <h1>hola mundooooooo</h1>
+      <h2>hola mundo 2 sin tailwind</h2>
+      </div>    
+    </>
+  )
+}
+
+export default App
