2baf39c6857b8f87c571611cf47d0a811b5196a4
Juan Jose Aranda <arandajuanjose17@gmail.com>
2024-02-02 11:18:15 -0300

    new

--- Project-Portfolio-Personal/src/App.tsx
+++ Project-Portfolio-Personal/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
       </h1>
+      <img src="../assets/viking bear minimalista.jpg" alt="logo viking bear" />
       <h1>hola mundooooooo</h1>
       <h2>hola mundo 2 sin tailwind</h2>
       </div>    
