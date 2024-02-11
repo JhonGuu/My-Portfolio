d7a9d747fb67dc1cc472c41c7b35ca7a162473e9
Juan Jose Aranda <arandajuanjose17@gmail.com>
2024-02-10 22:32:17 -0300

    o

--- Project-Portfolio-Personal/src/components/Body.tsx
+++ Project-Portfolio-Personal/src/components/Body.tsx
@@ -1,27 +1,35 @@
-import foto from '../assets/foto-portada.jpg'
+import typescript from '../assets/Typescript_logo_2020.svg'
+import reactLogo from '../assets/react.svg'
+import TailwindCss from '../assets/tailwindcss.svg'
 const Body = () => {
   return (
     <main className='flex w-auto h-auto ' >
       <section className='justify-center inline-block text-center  w-30 align-top border-2  rounded-2xl hover:border-green-500 shadow-xl shadow-cyan-500/50 p-1 mb-4 px-4'>
-        <h2 className='text-white text-center m-1 p-1 font-bold text-4xl '>React</h2>
-        <img className= '' src={foto} width={400} alt="image-personal" />
-        <p className='text-white text-center m-1 p-1 font-bold text-2xl'>Java</p>
-        <p className='text-white text-center m-1 p-1 font-bold text-1xl'>Traini</p>
+        <a href="https://es.react.dev" className=' flex text-white  m-1 p-2 font-bold text-4xl'>React</a>        
+        <img className= '' src={reactLogo} width={300} alt="image-personal" />
+        <p className='text-white text-center m-1 p-1 font-bold text-2xl'>2024</p>
+        <p className='text-white text-center m-1 p-1 font-bold text-1xl'>Learning</p>
         <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
         <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-100" > 45%</div>
         </div>
       </section>
       <section className='justify-center inline-block text-center  w-30 align-top border-2  rounded-2xl hover:border-green-500 shadow-xl shadow-cyan-500/50 p-1 mb-4 px-4'>
-        <h2 className='text-white text-center m-1 p-1 font-bold text-4xl '>Tailwind-CSS</h2>
-        <img className= '' src={foto} width={400} alt="image-personal" />
-        <p className='text-white text-center m-1 p-1 font-bold text-2xl'>Web Developer - Front End</p>
-        <p className='text-white text-center m-1 p-1 font-bold text-1xl'>Traini</p>
+        <a href="https://tailwindcss.com" className=' flex text-white  m-1 p-2 font-bold text-4xl'>Tailwind CSS</a>        
+        <img className= '' src={TailwindCss} width={300} alt="image-personal" />
+        <p className='text-white text-center m-1 p-1 font-bold text-2xl'>2024</p>
+        <p className='text-white text-center m-1 p-1 font-bold text-1xl'>Learning</p>
+        <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-100" > 45%</div>
+        </div>
       </section>
-      <section className='justify-center inline-block text-center  w-30 align-top border-2  rounded-2xl hover:border-green-500 shadow-xl shadow-cyan-500/50 p-1 mb-4 px-4 '>
-        <h2 className='text-white text-center m-1 p-1 font-bold text-4xl '>Juan Jose Aranda</h2>
-        <img className= '' src={foto} width={400} alt="image-personal" />
-        <p className='text-white text-center m-1 p-1 font-bold text-2xl'>Web Developer - Front End</p>
-        <p className='text-white text-center m-1 p-1 font-bold text-1xl'>Traini</p>
+      <section className='justify-center inline-block text-center  w-30 align-top border-2  rounded-2xl hover:border-green-500 shadow-xl shadow-cyan-500/50 p-1 mb-4 px-4'>
+        <a href="https://es.react.dev" className=' flex text-white  m-1 p-2 font-bold text-4xl'>React</a>        
+        <img className= '' src={typescript} width={280} alt="image-personal" />
+        <p className='text-white text-center m-1 p-1 font-bold text-2xl'>2024</p>
+        <p className='text-white text-center m-1 p-1 font-bold text-1xl'>Learning</p>
+        <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-100" > 45%</div>
+        </div>
       </section>
     </main>
   );
--- Project-Portfolio-Personal/src/components/Header.tsx
+++ Project-Portfolio-Personal/src/components/Header.tsx
@@ -1,23 +1,79 @@
+import { useCallback, useState } from 'react';
+import clsx from 'clsx';
 import viking from '../assets/logo-minimalista-blanco.png'
-const Header = () => {
-    return (
-     <div className=" flex w-[1920px] xsm:w-full mx-auto">
-      <header>          
-       <nav className=" flex lg:justify-start items-center content-center p-7 m-7">
-        <div className="logo flex-initial p-2 w-1/6">
-          <img src={viking}  alt="vikingbear" />
-        </div>  
-        <div className="links lg:flex hidden text-right w-1/6 md:4/6 ">        
-          <ul className=' flex menu items-center justify-center gap-5'>          
-            <li className="text-white  p-1 font-bold hover:text-green-400 "><a href="#">Home</a></li>
-            <li className="text-white  p-1 font-bold hover:text-green-400"><a href="#">About Me</a></li>
-            <li className="text-white  p-1 font-bold hover:text-green-400"><a href="#">Contacto</a></li>
-          </ul>
+
+const NavMenu = () => {
+  const [visibleMenu,setVisibleMenu] = useState(false);
+  const handleButttonMenu = useCallback( () => { 
+  setVisibleMenu(!visibleMenu)
+  },[visibleMenu]);
+return (
+  <header>       
+    <div className="mx-auto">
+      <nav className="flex justify-between lg:justify-start items-center">
+        <div className="logo p-2 w-1/6 m-4">
+          <img src={viking} width={400} alt="" />
         </div>
-       </nav>   
-      </header>
-     </div>
-    );
+        <div className='links lg:block hidden w-4/6 md:4/6'>
+          <ul className='menu flex items-center justify-center gap-5 '>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400'>About Me</a></li>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400'>Skills</a></li>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400'>Projects</a></li>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400'>Contact</a></li>            
+          </ul>  
+        </div>
+        <div className='block lg:hidden w-1/6 lg:w-4/6 p-10 m-10 '>
+          <button className='text-white text-xl border-solid' onClick={handleButttonMenu}>Menu</button>
+          <ul className={clsx('w-full absolute z-50 left-0 text-center bg-teal-400 p-4',visibleMenu ? 'visible' : 'hidden')}>
+          <li><a href="" className='text-white text-lg border-solid'>About Me</a></li>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400 '>Skills</a></li>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400'>Projects</a></li>
+            <li><a href="" className='text-white text-lg font-bold hover:text-green-400'>Contact</a></li>            
+          </ul>  
+        </div>
+      </nav>        
+    </div>
+  </header>  
+            );
+    
 };
 
-export default Header;
+
+export default NavMenu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import viking from '../assets/logo-minimalista-blanco.png'
+// const Header = () => {
+//     return (
+//      <div className=" flex w-[1920px] xsm:w-full mx-auto">
+//       <header>          
+//        <nav className=" flex lg:justify-start items-center content-center p-7 m-7">
+//         <div className="logo flex-initial p-2 w-1/6">
+//           <img src={viking}  alt="vikingbear" />
+//         </div>  
+//         <div className="links lg:flex hidden text-right w-1/6 md:4/6 ">        
+//           <ul className=' flex menu items-center justify-center gap-5'>          
+//             <li className="text-white  p-1 font-bold hover:text-green-400 "><a href="#">Home</a></li>
+//             <li className="text-white  p-1 font-bold hover:text-green-400"><a href="#">About Me</a></li>
+//             <li className="text-white  p-1 font-bold hover:text-green-400"><a href="#">Contacto</a></li>
+//           </ul>
+//         </div>
+//        </nav>   
+//       </header>
+//      </div>
+//     );
+// };
+
+// export default Header;
